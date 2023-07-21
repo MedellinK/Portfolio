275010170cedccbed69e40272071bbc0255d197c
Kelvyn Medellin <medellinkelvyn98@gmail.com>
2023-07-21 11:30:32 -0500

    Fixed my navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -4,12 +4,19 @@ import './Navbar.css';
 import { Link } from 'react-router-dom';
 export default function Navbar() {
   return (
-    <div className="navBar">
-      <h1>Home Main</h1>
-      <Link to="/">Home</Link>
-      <Link hre="#">About Me</Link>
-      <Link hre="#">Projects</Link>
-      <Link hre="#">Contact Me</Link>
+    <div className="navBar container">
+        <div className="row">
+            <div className="column">
+                <h1>Home Main</h1>
+
+            </div>
+            <div className="column link-list">
+              <Link className="class" to="/">Home</Link>
+              <Link hre="#">About Me</Link>
+              <Link hre="#">Projects</Link>
+              <Link hre="#">Contact Me</Link>
+            </div>
+        </div>
     </div>
   )
 }
