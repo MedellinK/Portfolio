698867d11e32b4d3b31ea9329b7ce04a0f65ac94
Kelvyn Medellin <medellinkelvyn98@gmail.com>
2023-07-21 13:55:59 -0500

    Added dark mode to code work easier.

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -4,17 +4,16 @@ import './Navbar.css';
 import { Link } from 'react-router-dom';
 export default function Navbar() {
   return (
-    <div className="navBar container">
-        <div className="row">
+    <div className="container">
+        <div className="row Navbar">
             <div className="column">
                 <h1>Home Main</h1>
-
             </div>
             <div className="column link-list">
-              <Link className="class" to="/">Home</Link>
-              <Link hre="#">About Me</Link>
-              <Link hre="#">Projects</Link>
-              <Link hre="#">Contact Me</Link>
+              <Link className="class" to="/"><h2>Home</h2></Link>
+                <Link hre="#"><h2>About</h2></Link>
+                <Link hre="#"><h2>Projects</h2></Link>
+                <Link hre="#"><h2>Contact</h2></Link>
             </div>
         </div>
     </div>
